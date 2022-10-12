20c71d10d2767521009b09677e3a82667eb1d378
Marc Becker <maarc1@gmx.de>
2022-10-12 12:03:52 +0200

    Challenge 3 completed

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,25 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const { pathname } = useRouter();
+
   return (
     <StyledHeader>
       <StyledNav>
-        <Link href="/">Home</Link>
-        <Link href="/product">Product</Link>
-        <Link href="/team">Team</Link>
-        <Link href="/account">Account</Link>
+        <Link href="/">
+          <Anchor active={pathname === "/"}>Home</Anchor>
+        </Link>
+        <Link href="/product">
+          <Anchor active={pathname === "/product"}>Produkt</Anchor>
+        </Link>
+        <Link href="/team">
+          <Anchor active={pathname === "/team"}>Team</Anchor>
+        </Link>
+        <Link href="/account">
+          <Anchor active={pathname === "/account"}>Account</Anchor>
+        </Link>
       </StyledNav>
     </StyledHeader>
   );
@@ -16,10 +27,18 @@ export default function Header() {
 
 const StyledHeader = styled.header`
   background: DarkSlateGray;
-  padding: 1.5rem;
+  padding: 0;
 `;
 
 const StyledNav = styled.nav`
   display: flex;
   justify-content: space-between;
+  justify-content: center;
+`;
+
+const Anchor = styled.a`
+  background-color: ${(props) => (props.active ? "green" : "none")};
+  width: 100%;
+  text-align: center;
+  padding: 20px;
 `;
